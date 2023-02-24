ea4e65387ab284bc77269f19b4d78f3401aca8f7
dgotlieb <Dgotlieb8@gmail.com>
2023-02-24 13:39:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print(123)
+from unittest import TestCase
+
+
+class TestTranslatePage(TestCase):
+    def setUp(self):
+        print('setting up')
+
+    def test_alert(self):
+        assert 1 == 1
+
+    def tearDown(self):
+        print('setting up')
